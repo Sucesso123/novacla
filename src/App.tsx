62dc810fc99f5daea46627463b5498ca02cc7631
Sucesso123 <walkerque300@gmail.com>
2026-09-23 16:09:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import {
 import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
 
-const WHATSAPP_NUMBER = '55119652888229';
+const WHATSAPP_NUMBER = '5511965288229';
 
 function createWhatsAppLink(message: string) {
   return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
